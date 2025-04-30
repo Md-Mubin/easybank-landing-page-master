843749686d5f3f080e27edb03152a71ef86dddbb
Md Mubin <fontdevmubin@gmail.com>
2025-04-30 23:42:55 +0600

    Changes to be committed: 	modified:   src/components/Articles.jsx

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 
 const Articles = () => {
-  return (
-    <div>Articles</div>
-  )
+
+    const articleCards = [
+        {
+            id : "1",
+            image : "/images/",
+            writer : "",
+            heading : "",
+            infos : ""
+        },
+
+        {
+            id : "1",
+            image : "/images/",
+            writer : "",
+            heading : "",
+            infos : ""
+        },
+
+        {
+            id : "1",
+            image : "/images/",
+            writer : "",
+            heading : "",
+            infos : ""
+        },
+
+        {
+            id : "1",
+            image : "/images/",
+            writer : "",
+            heading : "",
+            infos : ""
+        },
+    ]
+
+    return (
+        <>
+            <section className='py-20 bg-[#fafafa]'>
+                <div className="container">
+                    <h2 className='text-[60px]'>Latest Articles</h2>
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default Articles
